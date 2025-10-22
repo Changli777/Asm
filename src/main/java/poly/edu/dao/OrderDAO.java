3286a01a74b71f08ec123d7e7b09e3e6f56bb277
tvghung <tvghung@gmail.com>
2025-10-22 15:06:28 +0700

    Luu lai cho chac

--- src/main/java/poly/edu/dao/OrderDAO.java
+++ src/main/java/poly/edu/dao/OrderDAO.java
@@ -1,6 +1,7 @@
 package poly.edu.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import poly.edu.entity.Order;
 import poly.edu.entity.User;
